6c7a68bc1fb2719a9bff85d238c227297c946723
david <fabric_lee@naver.com>
2026-05-04 15:59:59 +0900

    Feat: graph layer 구현

--- app/services/constraint_impact/__init__.py
+++ app/services/constraint_impact/__init__.py
@@ -1,4 +1,18 @@
 from services.constraint_impact.atoms import AssignmentAtom, DerivedAtom, build_assignment_atoms
+from services.constraint_impact.graph_builder import (
+    build_constraint_edges,
+    build_constraint_nodes,
+    build_graph_expansion_states,
+    build_primitive_rule_graph,
+    index_nodes_by_atom,
+    propagate_graph_states,
+)
+from services.constraint_impact.graph_nodes import (
+    ConstraintEdge,
+    ConstraintNode,
+    GraphExpansionState,
+    PropagationResult,
+)
 from services.constraint_impact.rule_compiler import (
     build_rule_compile_context,
     compile_builtin_rules,
@@ -29,10 +43,14 @@ from services.constraint_impact.snapshot_builders import (
 __all__ = [
     "AssignmentAtom",
     "CompiledRuleMasks",
+    "ConstraintEdge",
+    "ConstraintNode",
     "CurrentRosterAnalysis",
     "DisallowedShiftRule",
     "DerivedAtom",
+    "GraphExpansionState",
     "PrimitiveRuleBundle",
+    "PropagationResult",
     "RequiredShiftRule",
     "SemanticsSnapshot",
     "SimulationAction",
@@ -40,13 +58,19 @@ __all__ = [
     "ShiftCountBoundRule",
     "analyze_current_roster",
     "build_assignment_atoms",
+    "build_constraint_edges",
+    "build_constraint_nodes",
     "build_current_atoms_from_roster_system",
+    "build_graph_expansion_states",
+    "build_primitive_rule_graph",
     "build_rule_compile_context",
     "build_rule_masks",
     "build_semantics_snapshot_from_active_path",
     "build_semantics_snapshot_from_roster_system",
     "compile_builtin_rules",
     "compile_personal_rules",
+    "index_nodes_by_atom",
     "merge_rule_bundles",
+    "propagate_graph_states",
     "simulate_action",
 ]
