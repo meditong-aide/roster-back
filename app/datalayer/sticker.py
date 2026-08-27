b1c44595820b5739ab2152b6c51953ff2624f031
HanJongjun <nade920110@gmail.com>
2026-08-27 17:12:13 +0900

    Feat : dev 백엔드가 운영 roster DB 를 읽고 쓰던 문제 + /push/list senderduty 추가

--- app/datalayer/sticker.py
+++ app/datalayer/sticker.py
@@ -1,21 +1,24 @@
+from datalayer.common import roster_db
+
+
 class sticker:
     @staticmethod
     def get_list():
-        _queryString = """
-               select OfficeCode, EmpSeqNo, stcker_date,sticker_contents from eun_roster.dbo.sticker where OfficeCode = %s and EmpSeqNo = %s and stcker_date =%s ;
+        _queryString = f"""
+               select OfficeCode, EmpSeqNo, stcker_date,sticker_contents from {roster_db()}.dbo.sticker where OfficeCode = %s and EmpSeqNo = %s and stcker_date =%s ;
                """
         return _queryString
 
     @staticmethod
     def insert_sticker():
-        _queryString = """
-          insert into eun_roster.dbo.sticker (OfficeCode, empseqno, stcker_date, sticker_contents ) VALUES (%s , %s , %s , %s ) ;
+        _queryString = f"""
+          insert into {roster_db()}.dbo.sticker (OfficeCode, empseqno, stcker_date, sticker_contents ) VALUES (%s , %s , %s , %s ) ;
           """
         return _queryString
 
     @staticmethod
     def delete_sticker():
-        _queryString = """
-         delete eun_roster.dbo.sticker where OfficeCode = %s and EmpSeqNo = %s and stcker_date =%s ;
+        _queryString = f"""
+         delete {roster_db()}.dbo.sticker where OfficeCode = %s and EmpSeqNo = %s and stcker_date =%s ;
          """
         return _queryString
